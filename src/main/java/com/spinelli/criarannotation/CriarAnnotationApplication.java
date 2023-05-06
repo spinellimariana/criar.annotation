46a29eeb1ad3cdabc5215bec21a631c41d498bb6
Mariana Spinelli <marispinelli@gmail.com>
2023-05-06 12:47:05 -0300

    teste

---
+++ src/main/java/com/spinelli/criarannotation/CriarAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.spinelli.criarannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriarAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriarAnnotationApplication.class, args);
+	}
+
+}
